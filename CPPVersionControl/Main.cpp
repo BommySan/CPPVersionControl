3a8a90616b4b1e38e51888c851c9cf0bee446316
Jonathan Bomysoad <JonBomysoad@gmail.com>
2023-02-16 00:03:24 -0500

    Added greet()

--- CPPVersionControl/Main.cpp
+++ CPPVersionControl/Main.cpp
@@ -2,9 +2,14 @@
 #include <string>
 using namespace std;
 
+void greet()
+{
+//Print to screen.
+	cout << "Hello and Welcome!" << endl << endl;
+}
+
 int main()
 {	
-	//Print to screen.
-	cout << "Hello and Welcome!" << endl << endl;
+	greet();
 
 }
